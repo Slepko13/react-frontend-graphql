8eb3799c58b57cf31826b65bac1bf3470991b8a9
mslep <v-mslepko@expediagroup.com>
2023-01-03 15:29:59 +0200

    feat(front): css sccs loaders

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './App.scss';
 
 const App = () => {
-  return <h1>Hello React</h1>;
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  );
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,37 @@
 const path = require('path');
 
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 module.exports = {
+  mode,
+
   entry: './src/index.js',
 
   output: {
     path: path.join(__dirname, '/dist'),
     filename: 'bundle.js',
   },
+  devtool: 'source-map',
 
   plugins: [
     new HTMLWebpackPlugin({
       template: './src/index.html',
     }),
+    new MiniCssExtractPlugin(),
   ],
 
   module: {
     rules: [
       {
-        test: /.js$/,
+        test: /\.s?css/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -29,5 +41,9 @@ module.exports = {
         },
       },
     ],
+  },
+
+  devServer: {
+    hot: true,
   },
 };
